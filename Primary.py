c46133f939f21ee1200b2762aa9bed3ba6c1d820
gaganrajn <54735027+gaganrajn@users.noreply.github.com>
2020-06-18 16:13:39 +0530

    Update Primary.py

--- Primary.py
+++ Primary.py
@@ -1,17 +1,26 @@
-import csv 
-import pprint 
+import csv
+import pprint
 from timeit import default_timer as timer 
 
-read_file = input("Enter the file path: ")
-start = timer() 
-with open(read_file) as csvfile: 
-    reader = csv. DictReader(csvfile)
+
+read_file='D:\chrome\c.csv'
+
+start = timer()
+with open(read_file) as csvfile:
+    reader = csv.DictReader(csvfile)
     field = reader.fieldnames
-    csv_rows = [] 
-    for row in reader: 
-        csv_rows.append({field[i]:row[field[i]] for i in range(len(field))})
-                          
+    csv_rows = []
+    for row in reader:
+        csv_rows.append(({field[i]:row[field[i]]  for i in range(len(field))}))
+        
 for i in csv_rows:
-    pprint. pprint(i)
-end = timer()    
-print("The time taken to convert from CSV to JSON format is :", str(end-start)+"s")
+    
+        pprint.pprint(i)
+        
+end = timer()
+print("The time taken to convert from .csv to .json format is:", end - start) 
+
+
+
+
+
